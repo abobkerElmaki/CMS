1384bb96dc9baf305b028881bdd663bfcde0cf3f
abobker Elmaki <bkry@gmial.com>
2019-02-25 16:38:23 +0300

    division splashscreen page  and configure Route

--- CMS/lib/main.dart
+++ CMS/lib/main.dart
@@ -1,14 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'Pages/LoginPage.dart';
-import 'Styles/color_const.dart';
-import 'Styles/gradient_const.dart';
+import 'Pages/SplachScreen.dart';
 void main() => runApp(
       new MaterialApp(
-
         routes: {
-        
           '/Login' :(context) => LoginScreen(),
         },
         theme: ThemeData(
@@ -20,87 +15,3 @@ void main() => runApp(
       ),
     );
 
-class SplachScreen extends StatefulWidget {
-  @override
-  _SplachScreenState createState() => _SplachScreenState();
-}
-
-class _SplachScreenState extends State<SplachScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Timer(Duration(seconds: 3), () => Navigator.pushNamed(context, '/Login'));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-    
-      body: Container (
-       decoration: BoxDecoration(gradient: SIGNUP_BACKGROUND),
-       child: new Stack(
-        
-        fit: StackFit.expand,
-      
-        children: <Widget>[
-          
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 50.0,
-                        child: Icon(
-                          Icons.local_hospital,
-                          color: Colors.green,
-                          size: 50.0,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 10.0),
-                      ),
-                      Text(
-                        "CMS",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                    ),
-                    Text(
-                      "online Appointment System",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-      ),
-    );
-  }
-}
